b83c59a385dd52df88dbec84bd75b376ab2af13e
Paul Kinlan <paulkinlan@google.com>
2012-02-06 12:31:12 +0000

    Fixes #10 - there are differences in .source on the regexp object between FF and Chrome, specifically FF will return the fully escaped string - this fix removes those differences

--- tests/routeparsing.js
+++ tests/routeparsing.js
@@ -6,17 +6,17 @@ describe("route parsing", function() {
 
   it("should parse / and add endmarker", function() {
     r.get("/", function() { });
-    expect(r.getRoutes()[0].regex.regexp.source).toEqual("/$");
+    expect(r.getRoutes()[0].regex.regexp).toEqual(/\//);
   });
 
   it("should parse basic route and leave untouched", function() {
     r.get("/category", function() { });
-    expect(r.getRoutes()[0].regex.regexp.source).toEqual("/category$");
+    expect(r.getRoutes()[0].regex.regexp).toEqual(/\/category$/);
   });
 
   it("should parse basic named identifier in position 0", function() {
     r.get("/:category", function(){});
     expect(r.getRoutes()[0].regex.groups["category"]).toEqual(0);
-    expect(r.getRoutes()[0].regex.regexp.source).toEqual("/([^/.\\\\]+)$");
+    expect(r.getRoutes()[0].regex.regexp).toEqual(/\/([^\/.\\\\]+)$/);
   });
 });
